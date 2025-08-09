dceb093c2b35728383319d8582ba6517dabc3106
Felipe Rios <feliperios665@gmail.com>
2025-08-08 19:38:14 -0500

    dependencias sin usar removidas

--- src/main/java/com/felop/model/entity/Cliente.java
+++ src/main/java/com/felop/model/entity/Cliente.java
@@ -1,9 +1,6 @@
 package com.felop.model.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
